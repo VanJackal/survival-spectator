77936cb27fac2029c6b732c5cf01d625d42bf296
jackal <v@njackal.com>
2024-07-17 13:36:12 -0500

    gamemode is swapped by command

--- src/main/java/com/njackal/SurvivalSpectator.java
+++ src/main/java/com/njackal/SurvivalSpectator.java
@@ -1,9 +1,17 @@
 package com.njackal;
 
+import com.mojang.brigadier.context.CommandContext;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
+import net.minecraft.world.GameMode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static net.minecraft.server.command.CommandManager.*;
 
 public class SurvivalSpectator implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
@@ -17,6 +25,35 @@ public class SurvivalSpectator implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("SurvivalSpectator initializing");
+		commandInit();
+	}
+
+	private void commandInit() {
+		LOGGER.info("Initializing Commands");
+
+		CommandRegistrationCallback.EVENT.register(((dispatcher, registryAccess, environment) -> dispatcher.register(literal("c").executes(this::onCCommand))));
+	}
+
+	private int onCCommand(CommandContext<ServerCommandSource> context) {
+		var source = context.getSource();
+		if(!source.isExecutedByPlayer()) {
+			source.sendFeedback(() -> Text.literal("This command can only be executed by a player."),false);
+			return 0;// exit early if the source isn't a player
+		}
+
+		var player = source.getPlayer();
+		assert player != null;
+		if(!player.isSpectator()) {
+			//todo save position
+			//todo save gamemode
+			player.changeGameMode(GameMode.SPECTATOR);
+		} else {
+			//todo put player back at old position
+			//todo set to original gamemode
+			player.changeGameMode(GameMode.SURVIVAL);
+		}
+
+		return 1;
 	}
 }
